e012130dc5850f7365ad8d9d0788ed96516ed46e
ruka0616 <ruka06260626.i@gmail.com>
2019-09-26 03:30:10 +0100

    LED点滅

---
+++ ledblink.py
@@ -0,0 +1,13 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14,GPIO.OUT)
+
+for i in range (10):
+    GPIO.output(14,GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(14,GPIO.LOW)
+    time.sleep(0.5)
+    
+GPIO.cleanup()
